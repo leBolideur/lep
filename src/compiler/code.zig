08baed91f4b7f5a26f2952aad721c5444ab0b62e
maxime <maxime.te@protonmail.com>
2024-04-22 19:03:24 +0200

    wip

--- src/compiler/code.zig
+++ src/compiler/code.zig
@@ -1,17 +1,39 @@
 const std = @import("std");
 
-const MAX_OPERAND = 3;
+const CodeError = error{ OpNoDefinition, ArrayListError, OutOfMemory };
 
 pub const Instructions = struct {
     instructions: []const u8,
 
-    pub fn string(self: Instructions, alloc: *std.mem.Allocator, definitions: *Definitions) []const u8 {
-        for (self.instructions, 1..) |instr, i| {
-            _ = i;
+    pub fn string(
+        self: Instructions,
+        alloc: *std.mem.Allocator,
+        definitions: *Definitions,
+    ) CodeError![]const u8 {
+        var offset: u8 = 0;
+        var buf: []const u8 = undefined;
+        // std.debug.print("instructions: {s}\n", .{self.instructions});
+
+        while (offset < self.instructions.len) {
+            const instr = self.instructions[offset];
+            // std.debug.print("instr: {c}\n", .{instr});
+
             const op_def = try definitions.*.lookup(@enumFromInt(instr));
-            const operands = read_operands(alloc, op_def.?, instr);
+            const def: OpDefinition = op_def orelse return CodeError.OpNoDefinition;
+            const width = def.operand_count;
+
+            const operands = self.instructions[(offset + 1)..(offset + width)];
+            // std.debug.print("\noperand: {any}\twidth: {d}", .{ operands[0], width });
+            // const fmt = try std.fmt.allocPrint(alloc.*, "{d:0>4} {s} {d}\n", .{ offset, def.name, operands[0] });
+            // for (fmt) |c| try str.append(c);
+            //
+            buf = try read_operands(alloc, def, operands);
+            std.debug.print("operands read: {d}\n", .{buf.len});
+
+            offset += width + 1; // +1 to jump opcode
         }
-        return "";
+
+        return buf;
     }
 };
 
@@ -22,8 +44,7 @@ pub const Opcode = enum(u8) {
 
 const OpDefinition = struct {
     name: []const u8,
-    // TODO: Understand that
-    operand_width: [MAX_OPERAND]u8 = [MAX_OPERAND]u8{ 0, 0, 0 },
+    operand_widths: []const u8,
     operand_count: u8,
 };
 
@@ -32,7 +53,7 @@ pub const Definitions = struct {
 
     pub fn init(alloc: *const std.mem.Allocator) !Definitions {
         var map = std.AutoHashMap(Opcode, OpDefinition).init(alloc.*);
-        try map.put(Opcode.OpConstant, OpDefinition{ .name = "OpConstant", .operand_count = 2 });
+        try map.put(Opcode.OpConstant, OpDefinition{ .name = "OpConstant", .operand_widths = &[_]u8{2}, .operand_count = 1 });
 
         return Definitions{
             .map = map,
@@ -48,29 +69,63 @@ pub const Definitions = struct {
     }
 };
 
-pub fn make(alloc: *const std.mem.Allocator, opcode: Opcode, operands: []const u8) ![]const u8 {
+pub fn make(alloc: *const std.mem.Allocator, opcode: Opcode, operands: []const usize) ![]const u8 {
     const definitions = try Definitions.init(alloc);
-    const def = definitions.map.get(opcode);
-    if (def == null) return &.{};
+    const def_ = definitions.map.get(opcode);
+    const def = def_ orelse return CodeError.OpNoDefinition;
 
-    const operand_count = def.?.operand_count;
-    const instr_ptr = try alloc.alloc(u8, operand_count + 1); // +1 for the opcode
+    var instr_len: u8 = 0;
+    for (def.operand_widths) |width| {
+        instr_len += width;
+    }
+    std.debug.print("operandS: {d}", .{operands});
+
+    const instr_ptr = try alloc.alloc(u8, instr_len + 1); // +1 for the opcode
     instr_ptr[0] = @intFromEnum(opcode);
 
-    for (operands, 0..operands.len) |operand, i| {
-        instr_ptr[i + 1] = operand;
+    var offset: u8 = 1;
+    for (operands, 0..) |operand, i| {
+        std.debug.print("i={d} - ", .{i});
+        const width = def.operand_widths[i];
+        switch (width) {
+            2 => {
+                instr_ptr[offset] = @as(u8, @intCast((operand & 0xFF00 >> 8)));
+                std.debug.print("1/ operand: {d} -> {d}\n", .{ operand, instr_ptr[offset] });
+                instr_ptr[offset + 1] = @as(u8, @intCast(operand & 0x00FF));
+                std.debug.print("2/ operand: {d} -> {d}\n", .{ operand, instr_ptr[offset + 1] });
+            },
+            else => unreachable,
+        }
+
+        offset += width;
     }
 
     return instr_ptr;
 }
 
-pub fn read_operands(alloc: *const std.mem.Allocator, def: OpDefinition, instr: []const u8) ![]const u8 {
-    var operands_ptr = try alloc.*.alloc(u8, def.operand_count);
+pub fn read_operands(
+    alloc: *const std.mem.Allocator,
+    def: OpDefinition,
+    instruction: []const u8,
+) CodeError![]const u8 {
+    var operands = std.ArrayList(u8).init(alloc.*);
+    // var operands = alloc.alloc(u8, def.operand_count);
 
-    for (0..def.operand_count) |i| {
-        const operand = instr[i];
-        operands_ptr[i] = operand;
+    var offset: u8 = 0;
+    for (def.operand_widths) |width| {
+        switch (width) {
+            2 => {
+                const operand = instruction[offset..2];
+                // operands.items[i] = operand;
+                for (operand) |b| {
+                    operands.append(b) catch return CodeError.ArrayListError;
+                }
+            },
+            else => unreachable,
+        }
+
+        offset += width;
     }
 
-    return operands_ptr;
+    return try operands.toOwnedSlice();
 }
--- src/compiler/code_test.zig
+++ src/compiler/code_test.zig
@@ -8,19 +8,20 @@ test "Test code.Make" {
     var alloc = arena.allocator();
 
     // Opcode, operands 2bytes, expected
-    const expected = [_]struct { Code.Opcode, [2]u8, [3]u8 }{
+    const expected = [_]struct { Code.Opcode, []const usize, []const u8 }{
         .{
             Code.Opcode.OpConstant,
-            [_]u8{ 254, 255 }, //65534
-            [3]u8{ @as(u8, @intCast(@intFromEnum(Code.Opcode.OpConstant))), 254, 255 },
+            &[_]usize{65534}, //65534
+            &[_]u8{ @as(u8, @intCast(@intFromEnum(Code.Opcode.OpConstant))), 255, 254 },
         },
     };
 
     for (expected) |exp| {
-        const instr = try Code.make(&alloc, exp[0], &exp[1]);
+        const instr = try Code.make(&alloc, exp[0], exp[1]);
+        std.debug.print("instr: {d}\tinstr len: {d}\n", .{ instr, instr.len });
         if (instr.len != exp[2].len) {
             std.debug.print(
-                "instruction has wrong length. want={d}, got={d}",
+                "\ninstruction has wrong length. want={d}, got={d}\n",
                 .{ exp[2].len, instr.len },
             );
         }
@@ -28,7 +29,7 @@ test "Test code.Make" {
         for (exp[2], 0..) |byte, i| {
             // std.debug.print("byte > exp: 0x{x}, instr: 0x{x} at pos {d}\n", .{ byte, instr[i], i });
             if (byte != instr[i]) {
-                std.debug.print("wrong byte at pos {d}. want={d}, got={d}", .{ i, byte, instr[i] });
+                std.debug.print("\nwrong byte at pos {d}. want={d}, got={d}\n", .{ i, byte, instr[i] });
             }
         }
     }
@@ -40,15 +41,15 @@ test "Test MiniDisassembler" {
     var alloc = arena.allocator();
 
     const expected =
-        \\\0000 OpConstant 1
-        \\\0003 OpConstant 2
-        \\\0006 OpConstant 65535
+        \\0000 OpConstant 1
+        \\0003 OpConstant 2
+        \\0006 OpConstant 65535
     ;
 
     const instructions = [3][]const u8{
-        try Code.make(&alloc, Code.Opcode.OpConstant, &[1]u8{1}),
-        try Code.make(&alloc, Code.Opcode.OpConstant, &[1]u8{2}),
-        try Code.make(&alloc, Code.Opcode.OpConstant, &[2]u8{ 254, 255 }),
+        try Code.make(&alloc, Code.Opcode.OpConstant, &[1]usize{1}),
+        try Code.make(&alloc, Code.Opcode.OpConstant, &[1]usize{2}),
+        try Code.make(&alloc, Code.Opcode.OpConstant, &[1]usize{65535}),
     };
 
     var flattened_ = std.ArrayList(u8).init(alloc);
@@ -60,7 +61,7 @@ test "Test MiniDisassembler" {
     const flattened = Code.Instructions{ .instructions = try flattened_.toOwnedSlice() };
 
     var definitions = try Code.Definitions.init(&alloc);
-    const str = flattened.string(&alloc, &definitions);
+    const str = try flattened.string(&alloc, &definitions);
 
     try std.testing.expectEqualStrings(expected, str);
 }
@@ -71,26 +72,26 @@ test "Test read operands" {
     var alloc = arena.allocator();
 
     // opcode, operands, bytes read
-    const expected = [_]struct { Code.Opcode, [2]u8, usize }{
+    const expected = [_]struct { Code.Opcode, []const usize, usize }{
         .{
             Code.Opcode.OpConstant,
-            [_]u8{ 254, 255 }, //65534
+            &[_]usize{65534},
             2,
         },
     };
 
     for (expected) |exp| {
-        const instruction = try Code.make(&alloc, exp[0], &exp[1]);
+        const instruction = try Code.make(&alloc, exp[0], exp[1]);
 
         const definitions = try Code.Definitions.init(&alloc);
         const def = try definitions.lookup(exp[0]);
         try std.testing.expect(def != null);
 
-        const operands_read = try Code.read_operands(&alloc, def.?, instruction[1..]);
-        try std.testing.expectEqual(exp[2], operands_read.len);
+        const operands = try Code.read_operands(&alloc, def.?, instruction[1..]);
+        try std.testing.expectEqual(exp[2], operands.len);
 
-        for (exp[1], 0..) |operand, i| {
-            try std.testing.expectEqual(operand, operands_read[i]);
+        for (exp[1], operands) |exp_op, operand| {
+            try std.testing.expectEqual(exp_op, operand);
         }
     }
 }
